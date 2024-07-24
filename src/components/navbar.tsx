ea252928c8424a0c8b2b60e598c0cae6af289327
Lokii0007 <lokeshyadav1098@gmail.com>
2024-07-25 00:13:19 +0530

    a

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,17 @@
 "use client"
 
 import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function Navbar() {
     const { data: session } = useSession()
+    const router = useRouter()
     return (
         <div className='sticky p-5 h-[10vh] bg-gray-600 text-white flex justify-center items-center '>
             <div className="navbar flex items-center justify-between">
                 <div className="nav-left px-12">
-                    True feedback
+                    <div className='cursor-pointer' onClick={()=>router.push('/')} >True feedback</div>
                 </div>
                 <div className="nav-right flex px-12">
                     {session ?
